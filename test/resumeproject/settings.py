c430aeaae3a4fdf16726c1fd8835b2d2bc3f6efe
Praful-Rathore-222 <73541555+Praful-Rathore-222@users.noreply.github.com>
2021-02-01 12:13:39 +0530

    Update settings.py

--- test/resumeproject/settings.py
+++ test/resumeproject/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.Whitenoise.Middleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
